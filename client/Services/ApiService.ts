8fb94aa1ba8a78e6184443653256558d1104030d
Jonathan <jonathan.raphael274@gmail.com>
2021-04-23 10:56:12 +0100

    refactor: client & server to  async/await

--- client/Services/ApiService.ts
+++ client/Services/ApiService.ts
@@ -1,16 +1,19 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import pathToBase64 from './ImageService';
 
 const apolloClient = new ApolloClient({
   uri: 'http://10.153.106.115:4000/',
   cache: new InMemoryCache()
 });
 
-const cramToApi = function (imageURI: String): void {
+const cramToApi = async function (imageURI: string): Promise<void> {
   console.log('uri: ', imageURI);
+  const imgBase64 = await pathToBase64(imageURI);
+  console.log('heres the encoded: ', imgBase64);
   apolloClient.query({
     query: gql`
 	    {
-	      topics(input: {title: "Current"}) {
+	      topics(input: {title: "Currednt"}) {
           id
         }
       }
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,5 +1,5 @@
 import { Topic } from './models/topics';
-import magic from './magic';
+// import magic from './magic';
 import visionApi from './visionApi';
 
 export const resolvers = {
@@ -8,8 +8,9 @@ export const resolvers = {
       const { input } = args;
       console.log('in the backend now', input);
       visionApi('https://v1.nitrocdn.com/kDXDIJNDOaszRbpagqNqUtquAQQkiLpO/assets/static/optimized/rev-ddee88e/wp-content/uploads/2020/03/Q1a_Atoms-Isotopes_Atomic-Structure__GCSE_AQA_Physics.png');
-      const output = magic(input);
-      return await Topic.find(output).exec();
+      // const output = magic(input);
+      const hardCode = {title: "Current"}
+      return await Topic.find(hardCode).exec();
     }
   },
 };
