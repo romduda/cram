69ae63fc97b4e491cf82736d0c3f792449de1c3c
Roman Duda <roman.duda@80000hours.org>
2021-04-30 18:25:21 +0100

    test(client): jest initial setup working

---
+++ client/App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree: any = renderer.create(<App />).toJSON();
+    console.log('tree', tree);
+    expect(tree.children.length).toBe(1);
+  });
+});
--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
+  console.log('babel.config.js called');
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-  };
+    // transform: {
+      // "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+    //   "^.+\\.tsx?$": "ts-jest",
+      // "^.+\\.[jt]sx?$": "babel-jest",
+    }
+  //   env: {
+  //     "test": {
+  //       "plugins": ["@babel/plugin-transform-modules-commonjs"]
+  //     }
 };
+
+
+
